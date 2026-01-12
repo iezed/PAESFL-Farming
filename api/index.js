1ddec17ff165c586fd91f45e447a8ad7d7439d1a
OHANA-WEB <cman40285@gmail.com>
2026-01-13 02:49:11 +0900

    Fix vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
 // Vercel serverless function entry point
 import app from '../server/index.js';
 
-// Export as Vercel serverless function handler
-export default (req, res) => {
-  return app(req, res);
-};
+// Export the Express app for Vercel serverless functions
+export default app;
